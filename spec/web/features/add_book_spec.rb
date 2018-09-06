7e3740952cac6a8156752b61b3b9059a08b93d2f
Chris Marques <test.code.debug@gmail.com>
2018-09-06 13:55:22 -0400

    Wrap up bookshelf demo

--- spec/web/features/add_book_spec.rb
+++ spec/web/features/add_book_spec.rb
@@ -16,6 +16,20 @@ describe 'Add a book' do
     end
 
     expect(current_path).to eq '/books'
-    expect(page).to include 'New Book'
+    expect(page).to have_content 'New Book'
+  end
+
+  it 'displays a list of errors when params contain errors' do
+    visit '/books/new'
+
+    within 'form#book-form' do
+      click_button 'Create'
+    end
+
+    expect(current_path).to eq '/books'
+
+    expect(page).to have_content 'There was a problem with your submission'
+    expect(page).to have_content 'Title must be filled'
+    expect(page).to have_content 'Author must be filled'
   end
 end
